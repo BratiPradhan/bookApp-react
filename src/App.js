1542126e3142fa3b3898106a33d9973accafab21
BratiPradhan <pradhanbrati@gmail.com>
2021-04-29 16:15:26 +0530

    BOOK APP: to practice useContext and local storage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axiox from 'axios';
+import {Table, Button} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state ={
+    books: []
+  }
+
+  componentWillMount() {
+    axiox.get('http://localhost:3000/books')
+    .then((response) => (
+      this.setState({
+        books: response.data
+      })
+    ));
+  }
+  render() {
+    let books = this.state.books.map((book) => {
+      return (
+        <tr key={book.id}>
+              <td>{book.id}</td>
+              <td>{book.title}</td>
+      <td>{book.rating}</td>
+              <td>
+                <Button color="success" size="sm" className="mr-4">Edit</Button>
+                <Button color="danger" size="sm">Delete</Button>
+              </td>
+            </tr>
+      )
+    }) 
+    return (
+      <div className="App container">
+        <Table>
+          <thread>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Rating</th>
+              <th>Actions</th>
+            </tr>
+          </thread>
+
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Book title</td>
+              <td>2.5</td>
+              <td>
+                <Button color="success" size="sm" className="mr-4">Edit</Button>
+                <Button color="danger" size="sm">Delete</Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+      );
+  }
 }
-
+ 
 export default App;
